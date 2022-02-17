75acec36f066f43dbbe4acb880ae7e161ee532b4
Thiago de Camargo <44476671+Thiagodessa41@users.noreply.github.com>
2022-02-17 17:07:15 -0300

    State
    Função mutável:

--- src/App.js
+++ src/App.js
@@ -1,49 +1,48 @@
-import React,{ Component } from 'react';
+import React, {Component} from "react";
 
-class Equipe extends Component{
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      nome:'Thiago',
+      contador: 0
+    }
+    this.aumentar = this.aumentar.bind(this);
+    this.diminuir = this.diminuir.bind(this);
+  }
+
+  aumentar(){
+    let state = this.state;
+    state.contador += 1;
+    this.setState(state);
+  }
+
+  diminuir(){
+    let state = this.state;
+    if(state.contador === 0){
+        alert("Ops chegou a zero!:");
+        return;
+    }
+    state.contador -= 1;
+    this.setState(state);
+  }
+
+
   render(){
     return(
       <div>
-        <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade} />
-        <Social/>
-          <hr/>  
+
+        <h1>Contador</h1>
+        <h2>
+          <button onClick={this.diminuir}>-</button>          
+            {this.state.contador}
+            <button onClick={this.aumentar} >+</button>
+        </h2>
+
       </div>
     )
   }
 }
-
-const Social = () => {
-  return(
-    <div>
-       <a>Facebook </a>
-       <a>LinkedIn </a>
-       <a>GitHub </a>
-    </div>
-  )
-}
-
-class Sobre extends Component{
-  render(){
-    return(
-      <div>
-        <h3>Olá sou o (a) {this.props.nome} </h3>
-        <h3>Cargo: {this.props.cargo}</h3>
-        <h3>Idade: {this.props.idade} </h3>
-      </div>
-    )
-  }
-}
-
-function App(){
-  return(
-    <div>
-      <h1>Conheça nossa equipe:</h1>
-      <Equipe nome="Thiago" cargo="Desenvolvedor" idade="38" />
-      <Equipe nome="Carlos" cargo="Analista de sistemas" idade="25" />
-    </div>
-  )
-}
-
 
 export default App;
-
