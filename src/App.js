567c57569149d09716e4e91bcf2562e9ad2ae45d
Thiago de Camargo <44476671+Thiagodessa41@users.noreply.github.com>
2022-02-22 17:16:43 -0300

    Renderização condicional

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import React, {Component} from "react";
 
-
-
 class App extends Component{
- 
-  constructor(props){
-    super(props);
-    this.state={
-          hora:'00:00:00'
-    };
-  }
 
-  componentDidMount(){
-    setInterval(()=>{
-      this.setState({hora: new Date().toLocaleTimeString()})
-    },1000);
-  }
-
-componentDidUpdate(){
-  console.log('atualizou')
+constructor(props){
+  super(props);
+  this.state =  {
+      status: false
+  };
+  this.sair=this.sair.bind(this);
+  this.entrar=this.entrar.bind(this);
 }
- 
+sair(){
+  this.setState({status: false});
+}
+entrar(){
+  this.setState({status: true});
+}
+
   render(){
     return(
-      <div>
-        <h1>Meu projeto {this.state.hora} </h1>
-
+      <div>        
+            {this.state.status?
+              <div>
+                <h2> Bem vindo ao sistema...</h2>  
+                <button onClick={this.sair}>Sair</button>     
+              </div>:
+              <div>
+                <h2>Olá visitante, faça o Login!</h2>
+                <button onClick={this.entrar}>Entrar no sistema</button>
+              </div>           
+             }        
       </div>
-    )
+    );
   }
+
 }
 
 export default App;
